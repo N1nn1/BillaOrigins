f69f4805b4f5f38737ea1f9c2c6b503f7b03fd95
Coda <cody.koelliker@outlook.com>
2024-08-04 17:13:58 -0600

    wattoian mostly done

--- src/main/java/codyhuh/billaorigins/content/power/wattoian/FreezeResistancePower.java
+++ src/main/java/codyhuh/billaorigins/content/power/wattoian/FreezeResistancePower.java
@@ -1,28 +1,26 @@
-package codyhuh.billaorigins.content.power;
+package codyhuh.billaorigins.content.power.wattoian;
 
 import io.github.edwinmindcraft.apoli.api.configuration.NoConfiguration;
 import io.github.edwinmindcraft.apoli.api.power.configuration.ConfiguredPower;
 import io.github.edwinmindcraft.apoli.api.power.factory.PowerFactory;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.vehicle.Boat;
 
-public class SinkBoatPower extends PowerFactory<NoConfiguration> {
+public class FreezeResistancePower extends PowerFactory<NoConfiguration> {
 
-    public SinkBoatPower() {
+    public FreezeResistancePower() {
         super(NoConfiguration.CODEC);
     }
 
     @Override
     protected void tick(NoConfiguration configuration, Entity entity) {
-        if (entity.isPassenger() && entity.getVehicle() instanceof Boat boat) {
-            boat.onAboveBubbleCol(true);
-
+        if (entity.getTicksFrozen() > 0) {
+            entity.setTicksFrozen(0);
         }
 
     }
 
     @Override
     public boolean canTick(ConfiguredPower<NoConfiguration, ?> configuration, Entity entity) {
-        return entity.isPassenger() && entity.getVehicle() instanceof Boat;
+        return entity.isFreezing();
     }
 }
--- src/main/java/codyhuh/billaorigins/registry/PowerRegistry.java
+++ src/main/java/codyhuh/billaorigins/registry/PowerRegistry.java
@@ -1,7 +1,9 @@
 package codyhuh.billaorigins.registry;
 
 import codyhuh.billaorigins.BillaOrigins;
-import codyhuh.billaorigins.content.power.SinkBoatPower;
+import codyhuh.billaorigins.content.power.wattoian.FreezeResistancePower;
+import codyhuh.billaorigins.content.power.wattoian.MountDeathPower;
+import codyhuh.billaorigins.content.power.wattoian.SinkBoatPower;
 import io.github.edwinmindcraft.apoli.api.power.factory.PowerFactory;
 import io.github.edwinmindcraft.origins.common.registry.OriginRegisters;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,4 +13,6 @@ public class PowerRegistry {
     public static final DeferredRegister<PowerFactory<?>> POWERS = DeferredRegister.create(OriginRegisters.POWER_FACTORIES.getRegistryName(), BillaOrigins.MOD_ID);
 
     public static final RegistryObject<SinkBoatPower> SINK_BOAT_POWER = POWERS.register("sink_boat", SinkBoatPower::new);
+    public static final RegistryObject<MountDeathPower> KILL_MOUNT_POWER = POWERS.register("kill_mount", MountDeathPower::new);
+    public static final RegistryObject<FreezeResistancePower> FREEZE_RESISTANCE = POWERS.register("freeze_resistance", FreezeResistancePower::new);
 }
